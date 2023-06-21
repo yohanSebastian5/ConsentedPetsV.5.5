174cc1cffcfe0df441856f833cf4b1bc366e9cb3
loren <loren@DESKTOP-T3VHGES>
2023-06-21 15:26:18 -0500

    arreglo1

--- ConsentedPetsV.2.0/Entidades/CLProductoE.cs
+++ ConsentedPetsV.2.0/Entidades/CLProductoE.cs
@@ -5,19 +5,13 @@ using System.Web;
 
 namespace ConsentedPetsV._2._0.Entidades
 {
-    public class CLProductoE
+    public class ClProductoE
     {
-        public int idCategoria{ get; set; }
-        public int idUsuario{ get; set; }
-        public int idTienda{ get; set; }
-        public string nombreP{ get; set; }
-        public string nombreC{ get; set; }
-        public string descripcionC{ get; set; }
-        public string descripcionP{ get; set; }
-        public string foto{ get; set; }
-        public string precio{ get; set; }
-        public string fecha{ get; set; }
-        public string cantidad{ get; set; }
-        public string total{ get; set; }
+        public int idProducto { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public string foto { get; set; }
+        public int precio { get; set; }
+        public int idCategoriaPS { get; set; }
     }
 }
