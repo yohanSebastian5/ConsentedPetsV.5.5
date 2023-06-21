088d7d32e9b57dc48c7a26d377146454f45bffa0
loren <loren@DESKTOP-T3VHGES>
2023-06-21 15:58:37 -0500

    arreglo Precio ClProductoE

--- ConsentedPetsV.2.0/Entidades/ClProductoE.cs
+++ ConsentedPetsV.2.0/Entidades/ClProductoE.cs
@@ -11,7 +11,18 @@ namespace ConsentedPetsV._2._0.Entidades
         public string nombre { get; set; }
         public string descripcion { get; set; }
         public string foto { get; set; }
-        public int precio { get; set; }
+        public int precioP { get; set; }
         public int idCategoriaPS { get; set; }
+        public int idCategoria { get; set; }
+        public int idUsuario { get; set; }
+        public int idTienda { get; set; }
+        public string nombreP { get; set; }
+        public string nombreC { get; set; }
+        public string descripcionC { get; set; }
+        public string descripcionP { get; set; }
+        public string precio { get; set; }
+        public string fecha { get; set; }
+        public string cantidad { get; set; }
+        public string total { get; set; }
     }
 }
