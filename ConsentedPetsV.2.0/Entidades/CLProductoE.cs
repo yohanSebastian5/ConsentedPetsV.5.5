4766e69b6ac56b69a937e230a196491a6db98d52
FernandaGomez79 <luiyifer79@gmail.com>
2023-07-31 07:33:42 -0500

    f

--- ConsentedPetsV.2.0/Entidades/CLProductoE.cs
+++ ConsentedPetsV.2.0/Entidades/CLProductoE.cs
@@ -24,6 +24,5 @@ namespace ConsentedPetsV._2._0.Entidades
         public string fecha { get; set; }
         public string cantidad { get; set; }
         public string total { get; set; }
-
     }
 }
